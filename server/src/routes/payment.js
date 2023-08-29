6d923abaf9bc0ff9b986e89dbb37ab46242acd5f
together320 <yuzheli320@gmail.com>
2023-08-29 09:33:11 -0700

    payment 3d completed

--- server/src/routes/payment.js
+++ server/src/routes/payment.js
@@ -2,20 +2,22 @@ import express from "express";
 import {
   process_hpp,
   process_2d,
-  process_3d,
-  process_3d_challenge,
   callback_transxnd_hpp,
   callback_mps,
   fetch_status,
   update_trans_status
 } from "../controllers/payment_controller.js";
+import {
+  process_3d_initiate,
+  process_3d_pay
+} from "../controllers/payment_3d_controller.js"
 
 const router = express.Router();
 
 router.post("/hpp", process_hpp);
 router.post("/2d", process_2d);
-router.post("/3d", process_3d);
-router.post("/3d/challenge", process_3d_challenge);
+router.post("/3d/initialize", process_3d_initiate);
+router.post("/3d/pay", process_3d_pay);
 router.get("/callbackTransxndHpp", callback_transxnd_hpp);
 router.post("/callbackMps", callback_mps);
 router.post("/status", fetch_status);
